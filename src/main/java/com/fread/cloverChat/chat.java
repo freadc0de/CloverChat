256d77044148a48e10193318fbdc401c8d831ac9
freadc0de <freadnight2@gmail.com>
2025-01-05 22:28:38 +0300

    update

---
+++ src/main/java/com/fread/cloverChat/chat.java
@@ -0,0 +1,17 @@
+package com.fread.cloverChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class chat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
